3f75f9c8ef0a175dd50bef01b4363960036f5e65
GerardAyg <aygun.gerard@gmail.com>
2023-02-13 10:50:00 +0100

    ajout de la classe reservation

---
+++ src/main/java/com/inti/entities/Reservation.java
@@ -0,0 +1,66 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "RESERVATION", schema = "gestion_agenceVoyage_app")
+public class Reservation implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idVoyageur;
+
+	@Temporal(TemporalType.DATE)
+	private Date dateReservation;
+
+	private int nbJours;
+
+	public Reservation() {
+
+	}
+
+	public Reservation(Date dateReservation, int nbJours) {
+		this.dateReservation = dateReservation;
+		this.nbJours = nbJours;
+	}
+
+	public Long getIdVoyageur() {
+		return idVoyageur;
+	}
+
+	public void setIdVoyageur(Long idVoyageur) {
+		this.idVoyageur = idVoyageur;
+	}
+
+	public Date getDateReservation() {
+		return dateReservation;
+	}
+
+	public void setDateReservation(Date dateReservation) {
+		this.dateReservation = dateReservation;
+	}
+
+	public int getNbJours() {
+		return nbJours;
+	}
+
+	public void setNbJours(int nbJours) {
+		this.nbJours = nbJours;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [idVoyageur=" + idVoyageur + ", dateReservation=" + dateReservation + ", nbJours=" + nbJours
+				+ "]";
+	}
+
+}
