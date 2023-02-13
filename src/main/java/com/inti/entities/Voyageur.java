8304f9ebf27a5cab62d3fd7cb7e5f14558f52699
mlaurent002 <laurent.melanie@hotmail.com>
2023-02-13 10:34:21 +0100

    Création Class Voyageur

---
+++ src/main/java/com/inti/entities/Voyageur.java
@@ -0,0 +1,66 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Voyageur implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idVoyageur;
+	private String nomVoyageur;
+	private String prenomVoyageur;
+	private int ageVoyageur;
+	
+	public Voyageur() {
+	}
+
+	public Voyageur(String nomVoyageur, String prenomVoyageur, int ageVoyageur) {
+		this.nomVoyageur = nomVoyageur;
+		this.prenomVoyageur = prenomVoyageur;
+		this.ageVoyageur = ageVoyageur;
+	}
+
+	public Long getIdVoyageur() {
+		return idVoyageur;
+	}
+
+	public void setIdVoyageur(Long idVoyageur) {
+		this.idVoyageur = idVoyageur;
+	}
+
+	public String getNomVoyageur() {
+		return nomVoyageur;
+	}
+
+	public void setNomVoyageur(String nomVoyageur) {
+		this.nomVoyageur = nomVoyageur;
+	}
+
+	public String getPrenomVoyageur() {
+		return prenomVoyageur;
+	}
+
+	public void setPrenomVoyageur(String prenomVoyageur) {
+		this.prenomVoyageur = prenomVoyageur;
+	}
+
+	public int getAgeVoyageur() {
+		return ageVoyageur;
+	}
+
+	public void setAgeVoyageur(int ageVoyageur) {
+		this.ageVoyageur = ageVoyageur;
+	}
+
+	@Override
+	public String toString() {
+		return "Voyageur [idVoyageur=" + idVoyageur + ", nomVoyageur=" + nomVoyageur + ", prenomVoyageur="
+				+ prenomVoyageur + ", ageVoyageur=" + ageVoyageur + "]";
+	}
+	
+}
